d7fc5a2c785b56e3267420c23b2cca6963d3e749
Nabin Koirala <nabinkoirala01.nk@gmail.com>
2023-08-13 02:27:05 +0545

    Add plugins to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -41,3 +41,29 @@ vim.api.nvim_set_keymap('n', '<leader>p', '"+p', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>P', '"+P', { noremap = true })
 vim.api.nvim_set_keymap('v', '<leader>p', '"+p', { noremap = true })
 vim.api.nvim_set_keymap('v', '<leader>P', '"+P', { noremap = true })
+
+require('plugins')
+
+-- setup orgmode
+-- init.lua
+
+-- Load custom treesitter grammar for org filetype
+require('orgmode').setup_ts_grammar()
+
+-- Treesitter configuration
+require('nvim-treesitter.configs').setup {
+  -- If TS highlights are not enabled at all, or disabled via `disable` prop,
+  -- highlighting will fallback to default Vim syntax highlighting
+  highlight = {
+    enable = true,
+    -- Required for spellcheck, some LaTex highlights and
+    -- code block highlights that do not have ts grammar
+    additional_vim_regex_highlighting = {'org'},
+  },
+  ensure_installed = {'org'}, -- Or run :TSUpdate org
+}
+
+require('orgmode').setup({
+    org_agenda_files = {'~/code/orgs/**/*'},
+    org_default_notes_file = '~/code/orgs/todo.org',
+})
